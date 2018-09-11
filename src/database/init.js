0f4b2608ccd0bdf922f55a4b7c490c88d12c308e
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-09-11 20:38:28 +0200

    init change

--- src/database/init.js
+++ src/database/init.js
@@ -6,30 +6,26 @@ var hashPassword = require("../auth/hashPassword").hashPassword;
 
 exports.init = callback => {
   log("Started database initialization");
-
-  var users = executeQuery("init/createTableUsers.sql");
-  var fixtures = executeQuery("init/createTableFixtures.sql");
-  var teams = executeQuery("init/createTableTeams.sql");
-  var bets = executeQuery("init/createTableBets.sql");
-  var globals = executeQuery("init/createTableGlobals.sql");
-
-  try{
-  Promise.all([users, fixtures, teams, bets, globals]).then(result => {
-    executeQuery("selects/getUserByLogin.sql", "admin").then(users => {
-      if (!users) {
-        var salt = genRandomString();
-        var hash = hashPassword("admin", salt);
-        var params = ["admin", "admin", hash, salt, null];
-        executeQuery("inserts/insertUser.sql", params).then(() => {
-          log("Admin account created");
+  executeQuery("init/createTableUsers.sql").then(resp => {
+    executeQuery("init/createTableFixtures.sql").then(resp => {
+      executeQuery("init/createTableTeams.sql").then(resp => {
+        executeQuery("init/createTableBets.sql").then(resp => {
+          executeQuery("init/createTableGlobals.sql").then(resp => {
+            executeQuery("selects/getUserByLogin.sql", "admin").then(users => {
+              if (!users) {
+                var salt = genRandomString();
+                var hash = hashPassword("admin", salt);
+                var params = ["admin", "admin", hash, salt, null];
+                executeQuery("inserts/insertUser.sql", params).then(() => {
+                  log("Admin account created");
+                });
+              }
+              log("Database initialized successfully");
+              callback();
+            });
+          });
         });
-      }
-      log("Database initialized successfully");
-      callback();
+      });
     });
   });
-  }
-  catch(err){
-    callback();
-  } 
 };
